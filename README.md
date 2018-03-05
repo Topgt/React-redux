3e961ea8dcfb49bdae0ad781eee1158dcaff4174
RumMac <topgt1@163.com>
2018-03-06 01:09:23 +0800

    feat: react-redux的使用

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # React-redux
+- demo1 是redux最基本的用法
+
+- demo2 是配合React-Redux 专用库 使用
--- demo2/src/TtRedux.js
+++ demo2/src/TtRedux.js
@@ -1,7 +1,42 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { connect, Provider } from 'react-redux'
+import registerServiceWorker from './registerServiceWorker';
 
-function TtRedux(prop) {
-  return (<h1>{prop.number}</h1>)
+const reducer = (state={ number:0 }, action) => {
+  if ('add' === action.type)
+    return {number: state.number + 1};
 }
 
-export default TtRedux
+const store = createStore(reducer)
+
+function TtRedux(props) {
+  return (
+    <div>
+      <h1>{props.number}</h1>
+      <button onClick={props.handler}> add </button>
+    </div>
+  )
+}
+
+const mapStateToProps = ( state={ number:0 } ) => {
+  return {
+    number: state.number
+  }
+}
+
+const mapDisPatchToProps = (dispatch) => {
+  return {
+    handler: e => dispatch({type: 'add'})
+  }
+}
+
+const App = connect(mapStateToProps, mapDisPatchToProps)(TtRedux)
+
+ReactDOM.render(
+  <Provider store={store} >
+    <App />
+  </Provider>,
+  document.getElementById('root'))
+registerServiceWorker()
