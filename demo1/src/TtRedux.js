2ff04d7dcbf5cb00f24213cfa4a4f2c7823dfaa1
RumMac <topgt1@163.com>
2018-03-13 23:39:48 +0800

    fix: 删除一些没用的文件

--- demo1/src/TtRedux.js
+++ demo1/src/TtRedux.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { createStore } from 'redux';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
 
 function TtRedux(prop) {
   return (<h1>{prop.number}</h1>)
 }
+const reducer = (state=0, action) => {
+  if ('add' === action.type)
+    return state += 1;
+}
+
+const store = createStore(reducer)
+
+const render = () => {
+  ReactDOM.render(
+    <div>
+      <TtRedux number={store.getState()} />
+      <button onClick={()=>store.dispatch({type: 'add'})} >加一</button>
+    </div>,
+    document.getElementById('root'));
+}
+store.subscribe(render);
+store.dispatch({
+  type: 'add'
+})
+registerServiceWorker();
+
 
 export default TtRedux
