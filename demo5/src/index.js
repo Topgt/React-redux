4decffcb1a41a95c2866d2321e8666e3d0607b2c
RumMac <topgt1@163.com>
2018-03-16 23:52:54 +0800

    feat: RR4配合react-router-redux使用

--- demo5/src/index.js
+++ demo5/src/index.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { Provider, connedt } from 'react-redux'
+import createHistory from 'history/createBrowserHistory'
+import { push, routerMiddleware, routerReducer, ConnectedRouter } from 'react-router-redux'
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const history = createHistory()
+const middleware = routerMiddleware(history)
+const initStore = {}
+const reducer = (state={ number:0 }, action) => {}
+
+const store = createStore(
+  combineReducers(
+    reducer,
+    routerReducer
+  ),
+  applyMiddleware(
+    middleware
+  )
+)
+
+
+
+
+
+
+ReactDOM.render(
+  <Provider store={store} >
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root'))
+registerServiceWorker()
